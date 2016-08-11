6c10f345900a249fa5d4bd245d76991d42b310b0
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2016-08-11 07:32:18 -0700

    Use binary tree instead of hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,127 @@
-use std::collections::HashMap;
 
 struct BlockDesc{
-    size : u8,
+    is_free : bool,
+    size : usize,
     start : usize,
     end : usize
+}
+
+impl BlockDesc{
+    fn new(is_free : bool, size : usize, start : usize, end : usize) -> Self{
+        BlockDesc{
+            is_free : is_free,
+            size : size,
+            start : start,
+            end : end
+        }
+    }
+}
+
+struct Node{
+    left : Option<Box<Node>>,
+    block_desc : BlockDesc,
+    right : Option<Box<Node>>,
 }
 
 struct BuddyAllocator{
     arena : Vec<u8>,
     heap_size : usize,
-    free_list : HashMap<u8, Vec<BlockDesc>>
+    min_block_size : usize,
+    blocks_tree : Vec<BlockDesc>
 }
 
 impl BuddyAllocator{
     fn new(heap_size : usize) -> Self{
-        let mut m = HashMap::new();
-        m.insert(4, vec![]);
-        m.insert(8, vec![]);
-        m.insert(16, vec![]);
-        m.insert(32, vec![]);
-        m.insert(64, vec![]);
         let mut v = Vec::with_capacity(heap_size);
         for i in 0..heap_size {
             v.push(0);
         }
         BuddyAllocator{
             arena : v,
+            min_block_size : 4,
             heap_size : heap_size,
-            free_list : m
+            blocks_tree : Vec::new()
         }
     }
 
     fn alloc<T>(&mut self) -> *mut T{
         let size_needed = std::mem::size_of::<T>();
-        let adjusted_size = BuddyAllocator::get_adjusted_size(size_needed);
-        if adjusted_size as usize > self.heap_size {
+        let adjusted_order = BuddyAllocator::get_adjusted_order(size_needed) as u8;
+        if adjusted_order as usize > self.heap_size {
             panic!("Out of memory!");
         }
 
-        println!("size needed : {}, rounded size : {}", size_needed, adjusted_size);
-        if self.free_list[&adjusted_size].is_empty(){
+        println!("size needed : {}, rounded size : {}", size_needed, adjusted_order);
+        let required_size = 2i32.pow(adjusted_order as u32) as usize;
 
-            
-        }
-        self.arena[0..4].as_mut_ptr() as *mut T 
+        let desc = self.get_block(required_size);
+
+        //self.arena[0..4].as_mut_ptr() as *mut T 
+        std::ptr::null_mut()
     }
 
-    fn get_adjusted_size(size : usize) -> u8{
-        let mut _s = 0u8;
+    fn get_block(&mut self, requested_size : usize) -> Option<&BlockDesc>{
+        if self.blocks_tree.is_empty(){
+            let mut start = 0;
+            let mut end = self.heap_size - 1;
+            let mut size = self.heap_size;
+
+            //create root
+            let block = BlockDesc::new(false,
+                                       size,
+                                       start,
+                                       end);
+            self.blocks_tree.push(block);
+            if size == requested_size{
+                return self.blocks_tree.last();
+            }
+
+            let mut left_child_index = 1;
+
+            //create left and right
+            loop{
+
+                let middle = size / 2;
+
+                let left = BlockDesc::new(true,
+                                          size,
+                                          start,
+                                          middle - 1);
+                self.blocks_tree.push(left);
+                /*if size == requested_size{
+                    self.blocks_tree.last_mut().is_free = false;
+                    return self.blocks_tree.last(); 
+                }*/
+
+                let right = BlockDesc::new(true,
+                                          size,
+                                          middle,
+                                          end);
+                self.blocks_tree.push(right);
+
+                if size == requested_size{
+                    self.blocks_tree.last_mut().unwrap().is_free = false;
+                    return Some(&self.blocks_tree[left_child_index]); 
+                }
+
+                left_child_index = 2 * left_child_index + 1;
+                size = middle;
+                end = middle - 1;
+            }
+        }
+        None
+    }
+
+    fn get_adjusted_order(size : usize) -> u8{
+        let mut order = 0u8;
         for i in 2u32..{
             let d = 2i32.pow(i);
             if d >= size as i32{
-                _s = d as u8;
+                order = i as u8;
                 break;
             }
         }
-        _s
+        order 
     }
 }
 
@@ -71,9 +137,9 @@ fn main() {
 
 
 #[test]
-fn test_get_adjusted_size(){
-    assert_eq!(BuddyAllocator::get_adjusted_size(19usize), 32u8);
-    assert_eq!(BuddyAllocator::get_adjusted_size(32usize), 32u8);
-    assert_eq!(BuddyAllocator::get_adjusted_size(1usize), 4u8);
-    assert_eq!(BuddyAllocator::get_adjusted_size(0usize), 4u8);
+fn test_get_adjusted_order(){
+    assert_eq!(BuddyAllocator::get_adjusted_order(19usize), 5u8);
+    assert_eq!(BuddyAllocator::get_adjusted_order(32usize), 5u8);
+    assert_eq!(BuddyAllocator::get_adjusted_order(1usize), 2u8);
+    assert_eq!(BuddyAllocator::get_adjusted_order(0usize), 2u8);
 }
